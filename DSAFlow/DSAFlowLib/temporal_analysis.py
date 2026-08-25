6965b6a8603965ba064a240019ec039120b9b200
Alexis Oziel Martínez Nava <54672463+OziieL17@users.noreply.github.com>
2026-08-24 20:03:03 -0600

    Add temporal analysis component

---
+++ DSAFlow/DSAFlowLib/temporal_analysis.py
@@ -0,0 +1,6 @@
+"""Contrast time-density curve analysis along vascular centerlines."""
+
+
+def time_density_curve(frames, coordinates):
+    """Extract a temporal contrast-intensity curve at supplied coordinates."""
+    raise NotImplementedError
