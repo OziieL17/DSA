a827034f0d9149236483d743d07ec58b6015a3ef
Alexis Oziel Martínez Nava <54672463+OziieL17@users.noreply.github.com>
2026-08-24 20:02:48 -0600

    Add preprocessing component

---
+++ DSAFlow/DSAFlowLib/preprocessing.py
@@ -0,0 +1,6 @@
+"""DSA temporal and spatial preprocessing."""
+
+
+def maximum_opacification(frames):
+    """Compute a maximum-opacification representation from a DSA cine stack."""
+    raise NotImplementedError
