e35f0fe854ddc7f0be5b428bdee68a1c48433a0d
Alexis Oziel Martínez Nava <54672463+OziieL17@users.noreply.github.com>
2026-08-24 20:03:22 -0600

    Add quality control component

---
+++ DSAFlow/DSAFlowLib/quality_control.py
@@ -0,0 +1,6 @@
+"""Quality-control metrics and confidence annotations."""
+
+
+def segmentation_qc(reference, prediction):
+    """Compute segmentation QC metrics once reference annotations are available."""
+    raise NotImplementedError
