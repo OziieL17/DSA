9d2964621c4a2fd313b3b2a010a88d7c32874a0c
Alexis Oziel Martínez Nava <54672463+OziieL17@users.noreply.github.com>
2026-08-24 20:03:13 -0600

    Add flow estimation component

---
+++ DSAFlow/DSAFlowLib/flow_estimation.py
@@ -0,0 +1,10 @@
+"""Research-only flow estimation from temporal DSA information."""
+
+
+def estimate_centerline_velocity(*args, **kwargs):
+    """Estimate contrast-propagation velocity along a centerline.
+
+    This is not an invasive pressure measurement and must be validated before
+    physiological interpretation.
+    """
+    raise NotImplementedError
