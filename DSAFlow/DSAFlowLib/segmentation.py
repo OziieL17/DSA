5e4014c5d9c6697c08e88ae5fcaa76e2a97c0dc0
Alexis Oziel Martínez Nava <54672463+OziieL17@users.noreply.github.com>
2026-08-24 20:02:53 -0600

    Add vessel segmentation component

---
+++ DSAFlow/DSAFlowLib/segmentation.py
@@ -0,0 +1,6 @@
+"""Cerebral vessel segmentation routines."""
+
+
+def segment_vessels(image, parameters=None):
+    """Create a vascular mask from a preprocessed DSA representation."""
+    raise NotImplementedError
