99050ae7677acaffa0e40aa97a3d923392140181
Alexis Oziel Martínez Nava <54672463+OziieL17@users.noreply.github.com>
2026-08-24 20:03:18 -0600

    Add visualization component

---
+++ DSAFlow/DSAFlowLib/visualization.py
@@ -0,0 +1,6 @@
+"""VTK/Slicer visualization of vascular models and derived scalar/vector data."""
+
+
+def apply_scalar_map(model, values, name):
+    """Attach a scalar array to a model for heat-map visualization."""
+    raise NotImplementedError
