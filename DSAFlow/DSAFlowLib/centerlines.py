13ce8c09bb80c01c86de67602e5c9fc5d51d84cc
Alexis Oziel Martínez Nava <54672463+OziieL17@users.noreply.github.com>
2026-08-24 20:02:57 -0600

    Add centerline component

---
+++ DSAFlow/DSAFlowLib/centerlines.py
@@ -0,0 +1,6 @@
+"""Centerline extraction and vascular graph representation."""
+
+
+def extract_centerlines(vessel_model):
+    """Extract centerlines from a vascular model, preferably through SlicerVMTK."""
+    raise NotImplementedError
