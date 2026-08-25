33786c669c9f295ab5898f264de2ba04c56851ef
Alexis Oziel Martínez Nava <54672463+OziieL17@users.noreply.github.com>
2026-08-24 20:03:07 -0600

    Add projection geometry component

---
+++ DSAFlow/DSAFlowLib/geometry.py
@@ -0,0 +1,6 @@
+"""Projection geometry and future multiview reconstruction utilities."""
+
+
+def projection_metadata(metadata):
+    """Normalize geometric metadata needed to model a DSA projection."""
+    raise NotImplementedError
